55acc44b894a1645cf716ec53947bb2d28072cf5
严状 <yanzhuang@2980.com>
2019-06-13 21:07:42 +0800

    add route Canvas

--- src/pages/index/App.vue
+++ src/pages/index/App.vue
@@ -2,9 +2,10 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/canvas">Canvas</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 <style lang="stylus">
@@ -17,9 +18,11 @@
 
 #nav
   padding 30px
+
   a
     font-weight bold
     color #2c3e50
+
     &.router-link-exact-active
       color #42b983
 </style>
